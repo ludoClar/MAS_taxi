ee69b1a444a624375b01198cad60fffb18cccc08
Ludovic <ludovic.clarissou@telecom-st-etienne.fr>
2017-01-04 12:20:24 +0100

    apparition random

--- test/src/test/Source.java
+++ test/src/test/Source.java
@@ -11,6 +11,7 @@ import repast.simphony.util.ContextUtils;
 public class Source extends Agent {
 	protected Coordonnees coordonnees;
 	protected int pourcentageBaby;
+	protected int nextClient;
 	int i = 0;
 	int step = 0;
 	int start = 0;
@@ -32,20 +33,25 @@ public class Source extends Agent {
 
 	}
 
-	@ScheduledMethod(start = 1, interval = 200, priority = 2)
+	@ScheduledMethod(start = 1, interval = 1, priority = 2)
 	public void implement() {
-		boolean baby = (new Random().nextInt()) % 100 + 1 <= pourcentageBaby ? true : false;
-		Customer a = new Customer(grid, space, baby);
-		int satisfaction = (int) ((Math.random() * 250) + 50);
-		a.setIDclient(start + i * step);
-		a.setCoordonnees(this.coordonnees);
-		a.setSatisfaction(satisfaction);
-		//System.out.println(a.getCoordonnees());
-		//System.out.println("In clients créés : ");
-		Context context = ContextUtils.getContext(this);
-		context.add(a);
-		space.moveTo(a, coordonnees.getX(), coordonnees.getY());
-		i++;
+		if (nextClient > 0)
+			nextClient--;
+		else {
+			nextClient = new Random().nextInt() % 10 + 350;
+			boolean baby = (new Random().nextInt()) % 100 + 1 <= pourcentageBaby ? true : false;
+			Customer a = new Customer(grid, space, baby);
+			int satisfaction = (int) ((Math.random() * 250) + 50);
+			a.setIDclient(start + i * step);
+			a.setCoordonnees(this.coordonnees);
+			a.setSatisfaction(satisfaction);
+			//System.out.println(a.getCoordonnees());
+			//System.out.println("In clients créés : ");
+			Context context = ContextUtils.getContext(this);
+			context.add(a);
+			space.moveTo(a, coordonnees.getX(), coordonnees.getY());
+			i++;
+		}
 	}
 
 	public void setCoordonnees(Coordonnees coordonnees) {
