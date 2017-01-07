b777b84b3367c5876e387200402feb77487155d6
Ludovic <ludovic.clarissou@telecom-st-etienne.fr>
2017-01-07 15:12:37 +0100

    before merged

--- test/src/test/Source.java
+++ test/src/test/Source.java
@@ -33,7 +33,8 @@ public class Source extends Agent {
 		this.start = start;
 	}
 
-	public Source(Grid<Agent> grid, ContinuousSpace<Agent> space, int pourcentageBaby, int satisfactionMin, int satisfactionStep) {
+	public Source(Grid<Agent> grid, ContinuousSpace<Agent> space, int pourcentageBaby, int satisfactionMin,
+			int satisfactionStep) {
 		super(grid, space);
 		this.pourcentageBaby = pourcentageBaby;
 		this.satisfactionMin = satisfactionMin;
@@ -46,12 +47,32 @@ public class Source extends Agent {
 			float ratio = -1;
 			int total = happyClients + angryClients;
 			if (total != 0)
-				ratio = (float)happyClients / total;
+				ratio = (float) happyClients / total;
 			System.out.println("Ratio from the source " + start + ": " + ratio);
 		}
 	}
 
-	/*--------------CLIENT CREATION-----------------*/
+	//	/*--------------CLIENT CREATION-----------------*/
+	//	@ScheduledMethod(start = 1, interval = 1, priority = 2)
+	//	public void implement() {
+	//		if (nextClient > 0) //even if this function is started at each tick, we don't want a client to appear every tick
+	//			nextClient--;
+	//		else {
+	//			nextClient = new Random().nextInt() % 10 + 350;
+	//			boolean baby = (new Random().nextInt()) % 100 + 1 <= pourcentageBaby ? true : false;
+	//			Customer a = new Customer(grid, space, baby);
+	//			int randSatisfaction = (int) ((Math.random() * satisfactionStep) + satisfactionMin);
+	//			a.setIDclient(start + i * step);
+	//			a.setCoordonnees(this.coordonnees);
+	//			a.setSatisfaction(randSatisfaction);
+	//			a.setOriginSource(this);
+	//			Context context = ContextUtils.getContext(this);
+	//			context.add(a);
+	//			space.moveTo(a, coordonnees.getX(), coordonnees.getY());
+	//			i++;
+	//		}
+	//	}
+
 	@ScheduledMethod(start = 1, interval = 1, priority = 2)
 	public void implement() {
 		if (nextClient > 0) //even if this function is started at each tick, we don't want a client to appear every tick
@@ -61,8 +82,19 @@ public class Source extends Agent {
 			boolean baby = (new Random().nextInt()) % 100 + 1 <= pourcentageBaby ? true : false;
 			Customer a = new Customer(grid, space, baby);
 			int randSatisfaction = (int) ((Math.random() * satisfactionStep) + satisfactionMin);
+			int sourceDest = (int) ((Math.random() * step));
+			Coordonnees coord = new Coordonnees(0, 0);
+			MooreQuery<Agent> query = new MooreQuery<Agent>(grid, this, 24, 24);
+			for (Agent o : query.query())
+				if (o instanceof Source) { //on observe chaque source
+					if (sourceDest > 0) {
+						coord = ((Source) o).getCoordonnees();
+					}
+					else
+						sourceDest--;
+				}
 			a.setIDclient(start + i * step);
-			a.setCoordonnees(this.coordonnees);
+			a.setCoordonnees(coord);
 			a.setSatisfaction(randSatisfaction);
 			a.setOriginSource(this);
 			Context context = ContextUtils.getContext(this);
@@ -71,45 +103,16 @@ public class Source extends Agent {
 			i++;
 		}
 	}
-	
-//	@ScheduledMethod(start = 1, interval = 1, priority = 2)
-//	public void implement() {
-//		if (nextClient > 0) //even if this function is started at each tick, we don't want a client to appear every tick
-//			nextClient--;
-//		else {
-//			nextClient = new Random().nextInt() % 10 + 350;
-//			boolean baby = (new Random().nextInt()) % 100 + 1 <= pourcentageBaby ? true : false;
-//			Customer a = new Customer(grid, space, baby);
-//			int randSatisfaction = (int) ((Math.random() * satisfactionStep) + satisfactionMin);
-//			int sourceDest = (int) ((Math.random() * step));
-//			Coordonnees coord = new Coordonnees(0, 0);
-//			MooreQuery<Agent> query = new MooreQuery<Agent>(grid, this, 24, 24);
-//			for (Agent o : query.query())
-//				if (o instanceof Source) { //on observe chaque source
-//					if (sourceDest>0)
-//					{
-//						 coord = ((Source) o).getCoordonnees();
-//					}
-//					else
-//						sourceDest--;
-//				}
-//			a.setIDclient(start + i * step);
-//			a.setCoordonnees(coord);
-//			a.setSatisfaction(randSatisfaction);
-//			a.setOriginSource(this);
-//			Context context = ContextUtils.getContext(this);
-//			context.add(a);
-//			space.moveTo(a, coordonnees.getX(), coordonnees.getY());
-//			i++;
-//		}
-//	}
-
 
 	public void setCoordonnees(Coordonnees coordonnees) {
 		this.coordonnees = coordonnees;
 	}
-	
-	public void happyClient(){happyClients++;};
-	
-	public void unhappyClient(){angryClients++;};
+
+	public void happyClient() {
+		happyClients++;
+	};
+
+	public void unhappyClient() {
+		angryClients++;
+	};
 }
